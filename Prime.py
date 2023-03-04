4695196706ea06d3e654e892b2297131fb28274e
aether <gabe.aether@gmail.com>
2023-03-03 20:08:45 -1000

    Added Prime.py

---
+++ Prime.py
@@ -0,0 +1,19 @@
+def is_prime_number(n):
+    """Returns True if n is prime, False otherwise"""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+x = int(input("Enter the number of prime numbers you want to generate: "))
+count = 0
+n = 2
+
+# loop until we have generated x prime numbers
+while count < x:
+    if is_prime_number(n):
+        print(n)
+        count += 1
+    n += 1
